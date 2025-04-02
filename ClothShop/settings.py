3ade81de142a00b2a1b30f1d56e57abb4e90113e
Gopika T G <gopikatg379@gmail.com>
2025-04-02 14:07:34 +0530

    modified2

--- ClothShop/settings.py
+++ ClothShop/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
