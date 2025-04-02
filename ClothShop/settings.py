35d1b9db6253b4c0a3f61166d0fdbef128f512ff
Gopika T G <gopikatg379@gmail.com>
2025-04-02 13:00:18 +0530

    modified1

--- ClothShop/settings.py
+++ ClothShop/settings.py
@@ -130,3 +130,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PORT = os.environ.get('PORT', '8000')
